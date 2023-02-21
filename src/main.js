23926d67323470d72f2ddbfa560d435e6ef73a40
yangzy <306525121@qq.com>
2023-02-21 11:09:58 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import router from "@/router";
+import 'element-plus/dist/index.css'
 
-createApp(App).mount('#app')
+
+
+const app = createApp(App)
+app.use(router)
+app.use(ElementPlus)
+app.mount('#app')
---
+++ src/views/Preview.vue
@@ -0,0 +1,11 @@
+<template>
+123
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
